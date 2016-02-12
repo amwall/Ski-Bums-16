feca39e295c51394470048ef2446a0a1e69c8862
Gareth <garethjones@uchicago.edu>
2016-02-12 09:09:18 -0600

    Data collected

--- database.py
+++ database.py
@@ -1,8 +1,28 @@
 import csv
+import pandas as pd
 
-def csv_writer(dictionary):
+
+def csv_writer(resort_list):
+    
+    keys = resort_list[0].keys()
+    with open('ski-resorts.csv', 'w') as csvfile:
+        resortwriter = csv.DictWriter(csvfile, keys)
+        resortwriter.writeheader()
+        resortwriter.writerows(resort_list)
+
+def create_dataframe(path):
+    
+    dataframe = pd.read_csv(path, engine='python', parse_dates=True)
+    # dataframe.to_pickle(pickle_name)
+    return dataframe
+    
+def load_dataframe(file_name):
+    
+    pass
+    
+    
         
-    with open('ski-resorts.csv', 'w', newline='') as f:
-        resortwriter = csv.writer(f, delimeter='|')
         
-        keys = dictionary
+        
+        
+        
