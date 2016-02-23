e3d8a4d60264d02d22fe257a9dc86dd79b5173d5
Alison Wall <amwall@cs.uchicago.edu>
2016-02-22 20:30:59 -0600

    Django stuff

---
+++ mysite/ski_bums/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SkiBumsConfig(AppConfig):
+    name = 'ski_bums'
---
+++ mysite/ski_bums/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.template import loader
+
+# Create your views here.
+
+# run python manage.py runserver 
+from django.http import HttpResponse
+
+
+def index(request):
+    info = {'parameters': ['Night Skiing', 'Terrain Parks', 'Beginner Slopes',
+                           'Intermediate Slopes', 'Advanced Slopes', 'Expert Slopes',
+                           'Amount of Runs', 'Amount of Skiable Terrain', 'Average Snowfall'],
+            }
+    #if request.method == 'POST':
+        #request.POST[]
+        # this is where we interact with the database
+
+    #else: 
+        # what to output when loading the page for the first time 
+
+    template = loader.get_template('ski_bums/index.html')
+
+    return HttpResponse(template.render(info, request))
+    # return render(request, 'ski_bums/index.html', info)
+
+
+        
