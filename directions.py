82435ca944ed52c39e3ca8538d6d956a65278305
Gareth <garethjones@uchicago.edu>
2016-03-06 20:46:26 -0600

    Directions file for sql.py

---
+++ directions.py
@@ -0,0 +1,41 @@
+def destination(addr, city, state, zip_code):
+    '''
+ 
+    '''
+    num_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    if addr[0] in num_list:
+        destination = addr.split()
+        destination = '+'.join(destination) + '+' + '+'.join(state.split())
+    elif city != '':
+        city = '+'.join(city.split())
+        state = '+'.join(state.split())
+        destination = city + '+' + state
+    else:
+        destination = zip_code
+
+    return destination
+    
+
+def travel_time_hours(addr, city, state, zip_code, current_location):
+    '''
+    Find the time it takes to drive from a user's current location 
+    to a resort. 
+    '''
+    current = current_location.split()
+    current = '+'.join(current)
+
+    end = destination(addr, city, state, zip_code)
+    
+    url = ('https://maps.googleapis.com/maps/api/distancematrix/json?' +
+            'origins=' + current + '&' + 'destinations=' + end +  
+            '&key=' + DISTANCE_MATRIX_ID)
+
+    url = urlopen(url)
+    text = url.read()
+    text = text.decode('utf-8')
+    values = re.findall('"value"\s:\s[0-9]+', text)
+    meters = float(re.findall('[0-9]+', values[0])[0])
+    minutes = float(re.findall('[0-9]+', values[1])[0])
+
+    return minutes / 60
