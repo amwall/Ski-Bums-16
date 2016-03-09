b6b6708398684ec67c0561969e372893a6c1f84c
Gareth <garethjones@uchicago.edu>
2016-03-08 19:13:09 -0600

    Unknown changes

--- preference.py
+++ preference.py
@@ -125,3 +125,32 @@ def score_runs(search_dict, parameters):
               (main.advanced * ?) + (main.expert * ?) AS run_score"
 
     return query, parameters
+
+def compute_time_between(lon1, lat1, lon2, lat2):
+
+    '''
+    Converts the output of the haversine formula to walking time in minutes
+    '''
+    
+    meters = haversine(lon1, lat1, lon2, lat2)
+    #adjusted downwards to account for manhattan distance
+    driving_speed_per_hr = 70 
+    hrs = meters / driving_speed_per_hr
+    return hrs
+
+def haversine(lon1, lat1, lon2, lat2):
+    '''
+    Calculate the circle distance between two points 
+    on the earth (specified in decimal degrees)
+    '''
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    # 6367 km is the radius of the Earth
+    km = 6367 * c
+    m = km * 1000
+    return m 
