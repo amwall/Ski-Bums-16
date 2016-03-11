bfeef992a7fa471842c993b22d80ec4184a7fdd6
Gareth <garethjones@uchicago.edu>
2016-03-11 13:15:04 -0600

    Network diagram created

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import seaborn as sb
 import plotly.offline as py
+from directions import compute_time_between, haversine
 
 
 PATH = "CSVs/ski-resorts.txt"
@@ -9,6 +10,7 @@ PATH = "CSVs/ski-resorts.txt"
 def read_data(PATH):
     
     df = pd.read_csv(PATH)
+    df.set_index(['ID'], inplace=True)
     return df
 
     
@@ -54,14 +56,62 @@ def snowfall_map(df):
     
     fig = dict(data=data, layout=layout )
     url = py.plot(fig, validate=False, filename='d3-ski-resorts' )
+        
+
+def network_map(cur_lat, cur_lon, df): 
+        
     
-    return url
-        
-
-def network_map(df): 
-        
-    pass
-        
-
-
+    resorts = [ dict(
+    type = 'scattergeo',
+    locationmode = 'USA-states',
+    lon = df['lon'],
+    lat = df['lat'],
+    hoverinfo = 'text',
+    text = df['name'],
+    mode = 'markers',
+    marker = dict( 
+        size=2, 
+        color='#1979D2',
+        line = dict(
+            width=2,
+            color='rgba(68, 68, 68, 0)'
+        )
+    ))]
     
+    paths = []
+    for i in range(1, len(df.index + 1)):
+        paths.append(
+            dict(
+                type = 'scattergeo',
+                locationmode = 'USA-states',
+                lon = [cur_lon, df['lon'][i] ],
+                lat = [cur_lat, df['lat'][i] ],
+                mode = 'lines',
+                line = dict(
+                    width = 1,
+                    color = '#21A14A',
+                ),
+                opacity = 0.5
+            )
+        )
+        
+    layout = dict(
+            title = 'Your location to Resorts <br> (Hover information)',
+            showlegend = False, 
+            geo = dict(
+                    scope = 'usa',
+                    projection = dict(type='albers usa'),
+                    showland = True,
+                    landcolor = "rgb(235,235,235)",
+                    subunitcolor = "rgb(200, 200, 200)",
+                    countrycolor = 'rgb(200, 200, 200)',
+                    countrywidth = 0.5,
+                    subunitwidth = 0.5     
+                    )
+            )
+        
+
+    fig = dict( data=paths + resorts, layout=layout )
+    url = py.plot(fig, filename='network.html')
+    
+        
