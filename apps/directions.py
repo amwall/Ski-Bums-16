7a87ebdd6059f0dbb37a001e5ead6f79c99aa644
Megan Wall <mewall@cs.uchicago.edu>
2016-03-14 22:51:04 -0500

    Adding comments

--- apps/directions.py
+++ apps/directions.py
@@ -2,11 +2,12 @@
 
 from urllib.request import urlopen
 import json
-from math import sin,cos,radians,asin, sqrt
+from math import sin,cos,radians,asin,sqrt
 
 DISTANCE_MATRIX_ID = 'AIzaSyDJ4p7topWHJW7SRAJJFY88BYVAapEkz0g'
 DIRECTIONS_ID = 'AIzaSyBkmUNSECcrSIPufRXJQCEm-0OhAmH9Mm8'
 GEOCODING_ID = 'AIzaSyCeg-uM3PsOT2ssRsPDfQdxZPbGz6k0kBc'#'AIzaSyB0Sx4EMq-IP2fXfzSyoRQ4-1llyKNJQgU'
+
 
 def get_lat_lon(location, locality=False):
     '''
@@ -45,28 +46,9 @@ def get_lat_lon(location, locality=False):
         return lat, lon, city, state
         
     return lat, lon
-
-def parse_destination(addr, city, state, zip_code):
-    '''
-    A helper function for parsing 
-    '''
-    int_list = list(range(10))
-    num_list = list(map(str,int_list)) # create a list of strings
-
-    if addr[0] in num_list:
-        destination = addr.split()
-        destination = '+'.join(destination) + '+' + '+'.join(state.split())
-    elif city != '':
-        city = '+'.join(city.split())
-        state = '+'.join(state.split())
-        destination = city + '+' + state
-    else:
-        destination = zip_code
-
-    return destination
     
 
-def get_distance(addr, city, state, zip_code, current_location):
+def get_distance(destination, current_location):
     '''
     Find the time it takes to drive from a user's current location 
     to a given location. 
@@ -74,10 +56,11 @@ def get_distance(addr, city, state, zip_code, current_location):
     current = current_location.split()
     current = '+'.join(current)
 
-    end = destination(addr, city, state, zip_code)
+    end = destination.split()
+    end = "+".join(end)
     
     url = ('https://maps.googleapis.com/maps/api/distancematrix/json?' +
-            'origins=' + current + '&' + 'destinations=' + end +  
+           'units=imperial' + '&origins=' + current + '&destinations=' + end +  
             '&key=' + DISTANCE_MATRIX_ID)
 
     url = urlopen(url)
@@ -92,12 +75,15 @@ def get_distance(addr, city, state, zip_code, current_location):
         time_text = data['rows'][0]['elements'][0]['duration']['text']
         dist_text = data['rows'][0]['elements'][0]['distance']['text']
     except:
-        seconds = 'nan'
-        meters = 'nan'
-        time_text = 'nan'
-        dist_text = 'nan'
+        seconds = 0
+        meters = 0
+        time_text = 'NaN'
+        dist_text = 'NaN'
+    
+    return time_text, dist_text
 
-def get_directions(addr, city, state, zip_code, current_location):
+
+def get_directions(destination, current_location):
     '''
     Given a user's current location and a resort, return
     a list with the directions
@@ -105,102 +91,79 @@ def get_directions(addr, city, state, zip_code, current_location):
     current = current_location.split()
     current = '+'.join(current)
 
-    end = destination(addr, city, state, zip_code)
+    end = destination.split()
+    end = "+".join(end)
     
     url = ('https://maps.googleapis.com/maps/api/directions/json?' +
             'origin=' + current + '&' + 'destination=' + end +  
             '&key=' + DIRECTIONS_ID)
-    
-    request = urlopen(url)
-    text = request.read()
-    text = text.decode('utf-8', errors='ignore')
-    data = json.loads(text)
-    
-    steps = data['routes'][0]['legs'][0]['steps']
-    
-    instructions = []
-    time = 0
-    dist = 0
-    for x in range(len(steps)):
-        instr = steps[x]['True_instructions']
-        instructions.append(instr)
-        dist += steps[x]['distance']['value']
-        time += steps[x]['duration']['value']
 
-    return instructions, time, distance
-    
-    # values = re.findall('("text"\s:\s)\"([0-9\.\sa-z]*)', text)
-    # time_travel = values[1][1]
-    # 
-    # distances = []
-    # for i in values[2::2]:
-    #     distances.append(i[1])
-    # 
-    # instructions = re.findall('("True_instructions"\s:\s)\"([A-Za-z0-9\\\/\s\-]*)', text)
-    # directions = []
-    # for i in instructions:
-    #     directions.append(i[1])
-    # 
-    # l = []
-    # for i in directions:
-    #     x = re.findall('([A-Za-z\s]+)([A-Za-z0-9\s\-]+)', i)
-    #     l.append(x)
-    # 
-    # directions = []
-    # for turn in l:
-    #     new = ''
-    #     first_term = turn[0][0]
-    #     if first_term == 'u':
-    #         for i in turn[1:len(turn) - 1:2]:
-    #             new += i[1][4:] + ' '
-    #     else:
-    #         new += first_term + ' '
-    #         for i in turn[2:len(turn) - 1:2]:
-    #             new += i[1][4:] + ' '
-    # 
-    #     directions.append(new)
-    # 
-    # direct_and_dist = []
-    # for i in range(len(directions)):
-    #     entry = directions[i] + 'and continue for ' + distances[i]
-    #     direct_and_dist.append(entry)
-    # 
-    # miles = distance_traveled(addr, city, state, zip_code, current_location)
-    # direct_and_dist.append('Total travel time is ' + time_travel)
-    # direct_and_dist.append('Total miles traveled: ' + str(miles) + 'mi')
-    # 
-    # return direct_and_dist
+    url = urlopen(url)
+    text = url.read()
+    text = text.decode('utf-8')
+
+    values = re.findall('("text"\s:\s)\"([0-9\.\sa-z]*)', text)
+    # Make a list of the distances you are driving for each step
+    distances = []
+    for i in values[2::2]:
+        distances.append(i[1])
+
+    instructions = re.findall('("html_instructions"\s:\s)\"([A-Za-z0-9\\\/\s\-]*)', 
+                              text)
+    directions = []
+    for i in instructions:
+        directions.append(i[1])
+    # Narrowing down the directions by using regular expressions
+    l = []
+    for i in directions:
+        x = re.findall('([A-Za-z\s]+)([A-Za-z0-9\s\-]+)', i)
+        l.append(x)
+    # Make a list of the directions (i.e. Turn right onto street, Merge onto...) 
+    directions = []
+    for turn in l:
+        new = ''
+        first_term = turn[0][0]
+        if first_term == 'u':
+            for i in turn[1:len(turn) - 1:2]:
+                new += i[1][4:] + ' '
+        else:
+            new += first_term + ' '
+            for i in turn[2:len(turn) - 1:2]:
+                new += i[1][4:] + ' '
+
+        directions.append(new)
+    # Make a final list of the directions with distance for each step
+    direct_and_dist = []
+    for i in range(len(directions)):
+        entry = directions[i] + 'and continue for ' + distances[i]
+        direct_and_dist.append(entry)
+
+    time_travel, dist_travel = get_distance(destination, current_location)
+    direct_and_dist.append('Total travel time is ' + time_travel)
+    direct_and_dist.append('Total miles traveled: ' + dist_travel)
+
+    return direct_and_dist
+
     
 def compute_time_between(lon1, lat1, lon2, lat2):
-
     '''
     Converts the output of the haversine formula to walking time in minutes
-    
-    ### This function was modified from PA3 to account for driving speeds ###
-    
     '''
-    # print("time check")
-    # print(lon1, lat1, lon2,lat2)
-    
     meters = haversine(lon1, lat1, lon2, lat2)
     #adjusted downwards to account for manhattan distance
-    driving_speed_per_hr = 50000
+    driving_speed_per_hr = 55000 #55km an hr
     hrs = meters / driving_speed_per_hr
-    # print("hrs",hrs)
+
     return hrs
+
 
 def haversine(lon1, lat1, lon2, lat2):
     '''
     Calculate the circle distance between two points 
     on the earth (specified in decimal degrees)
-    
-    #
-    # This function was copied from PA 3 
-    #
     '''
     # convert decimal degrees to radians 
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # print("check 1")
     # Haversine formula 
     dlon = lon2 - lon1 
     dlat = lat2 - lat1 
